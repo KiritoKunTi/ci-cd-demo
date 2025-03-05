a2cc0a4aa59251c6e08e16182671422ebc6e02a5
kiritokunti <duman200354@mail.ru>
2025-03-05 14:39:01 +0500

    Add deployment to CI pipeline

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log("CI/CD pipeline is working!");
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello, CI/CD!");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
